07628791b38d8c4853491653ea956a523a0f3484
Andrew Liu <andrewliu1324@yahoo.com>
2019-07-22 22:57:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <?php
+        include 'header.php';
+    ?>
+
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <center>
+
+            <h1>aliu1324.github.io</h1>    
+            <h2>Notes and projects on a variety of topics</h2>
+        
+        </center>
+
+
+
+</html>
